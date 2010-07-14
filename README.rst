ddb4f6e897e9d0d1748c7d15698486eb079f5b90
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-07-14 16:51:11 -0500

    Initial project structure.

---
+++ README.rst
@@ -0,0 +1,24 @@
+Django bindings to the Storymarket API
+======================================
+
+This library allows manual or automatic uploaded of Django objects to 
+`Stortmarket <http://storymarket.com/>`_.
+
+You'll need a Storymarket account to use this library, and you'll need to
+generate an API token by visiting the 
+`Developer API page <http://storymarket.com/users/api/>`_.
+
+Usage
+-----
+
+TODO
+
+Contributing
+------------
+
+Development takes place 
+`on GitHub <http://github.com/jacobian/django-storymarket>`_; please file
+bugs/pull requests there.
+
+Development on this project was funded by the 
+`Lawrence Journal-World <http://ljworld.com/>`_ - thanks!
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    
+setup(
+    name = "django-storymarket",
+    version = "1.0",
+    description = "Upload content stored in Django models to Storymarket.",
+    long_description = read('README.rst'),
+    url = 'http://packages.python.org/django-storymarket',
+    license = 'BSD',
+    author = 'Jacob Kaplan-Moss',
+    author_email = 'jacob@jacobian.org',
+    packages = find_packages(exclude=['tests']),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = ['django >= 1.2', 'python-storymarket'],
+    tests_require = ["mock"],
+    test_suite = "django_storymarket.runtests.runtests",
+)
